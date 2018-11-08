6d93145fdf923166b95320a7253734a149a4544a
zhbhun <zhbhun@gmail.com>
2018-11-08 09:56:38 +0800

    feat: 增加 postcss 的内置支持

--- src/index.js
+++ src/index.js
@@ -6,12 +6,26 @@ const withSass = require('../vendors/next-sass');
 const withNative = require('./withNative');
 
 const defaultConfig = require('./defaultConfig');
+
+const postcssPlugins = ({ browsers }) => {
+  const plugins = [require('autoprefixer')({ browsers })];
+  if (process.env.NODE_ENV !== 'development') {
+    plugins.push(
+      require('cssnano')({
+        preset: 'default',
+      })
+    );
+  }
+  return plugins;
+};
+
 /**
  * @param {Object} config
  * @param {String} config.distDir @see https://github.com/zeit/next.js/tree/7.0.2/#setting-a-custom-build-directory
  * @param {String} config.assetPrefix @see https://github.com/arefaslani/next-images#assetprefix
  * @param {Number} config.inlineImageLimit @see https://github.com/arefaslani/next-images#inlineimagelimit
  * @param {String} config.cssLocalIdentName @see https://github.com/zeit/next-plugins/tree/master/packages/next-css#with-css-modules-and-options
+ * @param {Object|Function} config.targets @see https://github.com/browserslist/browserslist
  * @param {Object|Function} config.native @see https://webpack.js.org/configuration/
  * @param {Object} config.webpack
  * @param {Object} config.env 根据环境读取不同配置
@@ -26,8 +40,14 @@ module.exports = (config = defaultConfig) => {
     assetPrefix,
     inlineImageLimit,
     cssLocalIdentName,
+    targets,
     native,
   } = merge({}, config, config.env[process.env.NODE_ENV]);
+  const postcssLoaderOptions = {
+    plugins: postcssPlugins({
+      browsers: targets.browsers,
+    }),
+  };
   return withPlugins(
     [
       [
@@ -45,6 +65,7 @@ module.exports = (config = defaultConfig) => {
             import: false,
             localIdentName: cssLocalIdentName,
           },
+          postcssLoaderOptions,
         },
       ],
       [
@@ -55,6 +76,7 @@ module.exports = (config = defaultConfig) => {
             import: false,
             localIdentName: cssLocalIdentName,
           },
+          postcssLoaderOptions,
         },
       ],
       [withNative, { native }],
